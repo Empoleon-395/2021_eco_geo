3c2b3ccb093c5c4146ecd53f0d5d1d01bd9ee318
Empoleon-395 <90383025+Empoleon-395@users.noreply.github.com>
2021-09-13 18:51:09 +0900

    Add files via upload

---
+++ task0/program/task0.py
@@ -0,0 +1,16 @@
+import os
+import matplotlib.pyplot as plt
+!pip install japanize_matplotlib
+import japanize_matplotlib
+import pandas as pd
+
+df=pd.read_csv("../data/longterm_pop.csv")
+
+fig=plt.figure(figsize=(6,3),dpi=300)
+fig.suptitle("2000年から2015年までの日本の人口推移")
+ax=fig.add_subplot(111)
+ax.plot(df["pop"])
+label=list(" ")
+label.extend(df["year"][list(range(0,len(df["year"]),2))])
+ax.set_xticklabels(label)
+fig.savefig("../data/pop_plot.png")
